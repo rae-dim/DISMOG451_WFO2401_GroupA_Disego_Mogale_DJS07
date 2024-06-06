08b5b075d9f575e79e31f747fca29d1ebb467f84
Disego <disegorachelm@gmail.com>
2024-06-06 21:05:06 +0200

    added labels for accessibility

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -4,15 +4,24 @@ export default function Meme() {
     return (
         <main>
             <form className='form'>
-                <input 
-                type='text'
-                placeholder='Top text'
-                className='form--input'
-                />
-                <input
-                type='text'
-                placeholder='Bottom text'
-                className='form--input'/>
+                <div>
+                    <label>Top text  {/* label added for accessibility */}
+                        <input 
+                        type='text'
+                        placeholder='Top text'
+                        className='form--input'
+                        />
+                    </label>
+                </div>
+
+                <div>
+                    <label>Bottom text
+                        <input
+                        type='text'
+                        placeholder='Bottom text'
+                        className='form--input'/>
+                    </label>
+                </div>
                 <button className='form--button'>Get a new meme image 🖼️</button>
             </form>
         </main>
