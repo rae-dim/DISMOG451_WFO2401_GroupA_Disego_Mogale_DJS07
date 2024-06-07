4e845eadb7d48ed57322451e522289cd907e8175
Disego <disegorachelm@gmail.com>
2024-06-07 05:03:18 +0200

    removie form tags

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -3,27 +3,25 @@ import React from 'react'
 export default function Meme() {
     return (
         <main>
-            <form className='form'>
-                <div>
-                    <label>Top text  {/* label added for accessibility */}
-                        <input 
-                        type='text'
-                        placeholder='Top text'
-                        className='form--input'
-                        />
-                    </label>
-                </div>
+            <div>
+                <label>Top text  {/* label added for accessibility */}
+                    <input 
+                    type='text'
+                    placeholder='Top text'
+                    className='form--input'
+                    />
+                </label>
+            </div>
 
-                <div>
-                    <label>Bottom text
-                        <input
-                        type='text'
-                        placeholder='Bottom text'
-                        className='form--input'/>
-                    </label>
-                </div>
+            <div>
+                <label>Bottom text
+                    <input
+                    type='text'
+                    placeholder='Bottom text'
+                    className='form--input'/>
+                </label>
                 <button className='form--button'>Get a new meme image 🖼️</button>
-            </form>
+            </div>
         </main>
        
     )
