5cf80b6e7ef2f9f5c394d326d0297577d0eb0c23
Disego <disegorachelm@gmail.com>
2024-06-07 13:17:52 +0200

    add memesData

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
+import React from 'react'
 
-
-export default function Header() {
+function Header() {
     return (
-        <header>
+        <div className="header">
             <img src='./img/troll-face.png'
             className="header--image"
             alt="logo"
             />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React</h4>
-        </header>
+        </div>
         
        
     )
 }
+
+export default Header
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -7,20 +7,20 @@ function Meme() {
     return (
         <main>
             <div className="form"> 
-                <label>Top text  {/* label added for accessibility */}
+                {/* <label>Top text  {/* label added for accessibility */} 
                     <input 
                     type='text'
                     placeholder='Top text'
                     className='form--input'
                     />
-                </label>
+                {/* </label> */}
     
-                <label>Bottom text
+                {/* <label>Bottom text */}
                     <input
                     type='text'
                     placeholder='Bottom text'
                     className='form--input'/>
-                </label>
+                {/* </label> */}
                 <button className='form--button'>Get a new meme image 🖼️</button>
             </div>
         </main>
